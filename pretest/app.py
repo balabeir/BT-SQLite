143784a00ef91e45aa8e02336a0bd46cb14aadd3
balabeir <beer32853@gmail.com>
2021-03-05 15:31:53 +0700

    pretest

--- pretest/app.py
+++ pretest/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = "Secret Key"
 
 # SqlAlchemy Database Configuration With sqlite3
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///crud.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myDatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
